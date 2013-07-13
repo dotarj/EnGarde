7a9557e673aff40db0c58a1cadc059b4e80ffbed
Arjen Post <mail@arjenpost.nl>
2013-07-13 08:07:55 +0200

    Code metrics demo

---
+++ samples/EnGarde.CodeMetrics.Demo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace EnGarde.CodeMetrics.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoArgumentValidation(1);
+            EnGardeChainedNegatingArgumentValidation(1);
+            EnGardeNonChainedNegatingArgumentValidation(1);
+            EnGardeChainedNonNegatingArgumentValidation(1);
+            EnGardeNonChainedNonNegatingArgumentValidation(1);
+            ClassicArgumentValidationCombined(1);
+            ClassicArgumentValidation(1);
+            ContractValidation(1);
+        }
+
+        private static int NoArgumentValidation(int i)
+        {
+            return i;
+        }
+
+        private static int EnGardeChainedNegatingArgumentValidation(int i)
+        {
+            Argument.Assert(i, "i")
+                .Not().IsLessThan(0)
+                .Not().IsGreaterThan(2);
+
+            return i;
+        }
+
+        private static int EnGardeNonChainedNegatingArgumentValidation(int i)
+        {
+            Argument.Assert(i, "i").Not().IsLessThan(0);
+            Argument.Assert(i, "i").Not().IsGreaterThan(2);
+
+            return i;
+        }
+
+        private static int EnGardeChainedNonNegatingArgumentValidation(int i)
+        {
+            Argument.Assert(i, "i")
+                .IsGreaterThan(0)
+                .IsLessThan(2);
+
+            return i;
+        }
+
+        private static int EnGardeNonChainedNonNegatingArgumentValidation(int i)
+        {
+            Argument.Assert(i, "i").IsGreaterThan(0);
+            Argument.Assert(i, "i").IsLessThan(2);
+
+            return i;
+        }
+
+        private static int ClassicArgumentValidation(int i)
+        {
+            if (i > 2)
+            {
+                throw new ArgumentOutOfRangeException("i", i, "");
+            }
+
+            if (i < 0)
+            {
+                throw new ArgumentOutOfRangeException("i", i, "");
+            }
+
+            return i;
+        }
+
+        private static int ClassicArgumentValidationCombined(int i)
+        {
+            if (i < 0 || i > 2)
+            {
+                throw new ArgumentOutOfRangeException("i", i, "");
+            }
+
+            return i;
+        }
+
+        private static int ContractValidation(int i)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(i <= 2);
+            Contract.Requires<ArgumentOutOfRangeException>(i >= 0);
+
+            return i;
+        }
+    }
+}
---
+++ samples/EnGarde.CodeMetrics.Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnGarde.CodeMetrics.Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Arjen Post")]
+[assembly: AssemblyProduct("EnGarde.CodeMetrics.Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dda2ddf7-47d1-45a5-856d-05776ab61ae4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
