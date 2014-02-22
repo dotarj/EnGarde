563b38eabe264e2e2ad4d0354d07ef4255b90d1e
Arjen Post <mail@arjenpost.nl>
2014-02-22 09:15:57 +0100

    IEnumerable<T> extension methods

---
+++ src/EnGarde/Argument{IEnumerable{T}}Extensions.cs
@@ -0,0 +1,133 @@
+﻿// Copyright (c) Arjen Post. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EnGarde
+{
+    public static partial class ArgumentExtensions
+    {
+        /// <summary>
+        /// Determines whether the argument value contains the specified item.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument to validate.</typeparam>
+        /// <param name="argument">A wrapper object containing the actual argument value.</param>
+        /// <param name="value">The object to locate in the <see cref="IEnumerable{T}"/>.</param>
+        /// <param name="message">The message that describes the error, if the validation of the argument value failed.</param>
+        /// <returns>The original wrapper object containing the actual argument value.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="argument"/> is null.</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="argument"/> value is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="argument"/> value contains the specified item (if negated).</exception>
+        /// <exception cref="ArgumentException"><paramref name="argument"/> value does not contain the specified item (if not negated).</exception>
+        [DebuggerStepThrough]
+        public static IValidatedArgument<IEnumerable<T>> Contains<T>(this IArgument<IEnumerable<T>> argument, T value, string message = null)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(ParameterNames.Argument);
+            }
+
+            if (argument.Value == null)
+            {
+                throw new InvalidOperationException(Messages.ArgumentValueIsNull);
+            }
+
+            Exception exception;
+
+            if (!ValidateContains(argument, value, message, out exception))
+            {
+                throw exception;
+            }
+
+            return argument.AsValidatedArgument();
+        }
+
+        internal static bool ValidateContains<T>(IArgument<IEnumerable<T>> argument, T value, string message, out Exception exception)
+        {
+            if (argument.Value.Contains(value))
+            {
+                if (argument.IsNegated)
+                {
+                    exception = new ArgumentException(message, argument.ParameterName);
+
+                    return false;
+                }
+            }
+            else
+            {
+                if (!argument.IsNegated)
+                {
+                    exception = new ArgumentException(message, argument.ParameterName);
+
+                    return false;
+                }
+            }
+
+            exception = null;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether the argument value contains items.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument to validate.</typeparam>
+        /// <param name="argument">A wrapper object containing the actual argument value.</param>
+        /// <param name="message">The message that describes the error, if the validation of the argument value failed.</param>
+        /// <returns>The original wrapper object containing the actual argument value.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="argument"/> is null.</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="argument"/> value is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="argument"/> value contains items (if negated).</exception>
+        /// <exception cref="ArgumentException"><paramref name="argument"/> value does not contain items (if not negated).</exception>
+        [DebuggerStepThrough]
+        public static IValidatedArgument<IEnumerable<T>> IsEmpty<T>(this IArgument<IEnumerable<T>> argument, string message = null)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(ParameterNames.Argument);
+            }
+
+            if (argument.Value == null)
+            {
+                throw new InvalidOperationException(Messages.ArgumentValueIsNull);
+            }
+
+            Exception exception;
+
+            if (!ValidateIsEmpty(argument, message, out exception))
+            {
+                throw exception;
+            }
+
+            return argument.AsValidatedArgument();
+        }
+
+        internal static bool ValidateIsEmpty<T>(IArgument<IEnumerable<T>> argument, string message, out Exception exception)
+        {
+            if (!argument.Value.Any())
+            {
+                if (argument.IsNegated)
+                {
+                    exception = new ArgumentException(message, argument.ParameterName);
+
+                    return false;
+                }
+            }
+            else
+            {
+                if (!argument.IsNegated)
+                {
+                    exception = new ArgumentException(message, argument.ParameterName);
+
+                    return false;
+                }
+            }
+
+            exception = null;
+
+            return true;
+        }
+    }
+}
---
+++ test/EnGarde.Test/Argument{IEnumerable{T}}ExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Arjen Post. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace EnGarde.Test
+{
+    [TestClass]
+    public class ArgumentIEnumerableExtensionsTests
+    {
+        [TestClass]
+        public class TheIsNullMethod
+        {
+            [TestMethod]
+            public void ShouldNotThrowExceptionIfValueContainsItem()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").Contains(1);
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentException))]
+            public void ShouldThrowExceptionIfValueContainsItem()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").Not.Contains(1);
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionIfValueDoesNotContainItem()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").Not.Contains(4);
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentException))]
+            public void ShouldThrowExceptionIfValueDoesNotContainItem()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").Contains(4);
+            }
+        }
+
+        [TestClass]
+        public class TheIsEmptyMethod
+        {
+            [TestMethod]
+            public void ShouldNotThrowExceptionIfValueIsEmpty()
+            {
+                // Arrange
+                var value = Enumerable.Empty<int>();
+
+                // Act
+                Ensure.That(value, "").IsEmpty();
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentException))]
+            public void ShouldThrowExceptionIfValueIsEmpty()
+            {
+                // Arrange
+                var value = Enumerable.Empty<int>();
+
+                // Act
+                Ensure.That(value, "").Not.IsEmpty();
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionIfValueIsNotEmpty()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").Not.IsEmpty();
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentException))]
+            public void ShouldThrowExceptionIfValueIsNotEmpty()
+            {
+                // Arrange
+                var value = Enumerable.Range(0, 3);
+
+                // Act
+                Ensure.That(value, "").IsEmpty();
+            }
+        }
+    }
+}
